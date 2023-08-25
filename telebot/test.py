766de1ce5b73b1d1d99c535645ec085acbaf350b
Udalov Pavel <udalovpavel95@gmail.com>
2023-08-26 01:35:32 +0500

    create big json answer in OneStringView request

--- telebot/test.py
+++ telebot/test.py
@@ -1,12 +1,12 @@
 import requests
 
 
-url = 'http://localhost:8000/one-string/'
+url = 'http://localhost:8000/one-string/?format=json'
 # response = requests.get(url=url)
 # data = response.json()
 # print("Type of request: ", type(data), "Data: ", data)
 
 text = {'textinput': 'test_text'}
 response = requests.post(url, data=text)
-print(response.content)
+print(response.json())
 
