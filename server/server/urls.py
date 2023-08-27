4a90fd171f362b1f3f5a85b19e0915dea5519d4a
Udalov Pavel <udalovpavel95@gmail.com>
2023-08-27 13:52:45 +0500

    repaire server

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,13 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hac_app.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
